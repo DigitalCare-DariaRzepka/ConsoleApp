373a5c0062cb5fab24eee2501d7c1aa60ec02ae9
Daria Rzepka <daria.rzepka@digitalcaregroup.com>
2023-05-17 13:22:07 +0200

    zmiana na 8

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(" ");
-for (int i = 0;i < 10; i++)
+for (int i = 0;i < 8; i++)
 {
     Console.WriteLine(i.ToString());
 }
